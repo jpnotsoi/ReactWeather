8226843691908731dcb5dadb118e1f9fca82e017
Wong <jin.pei.wong@accenture.com>
2017-05-29 18:05:09 +0800

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ var express = require('express');
 
 // create our app - pass express library
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+// check whether the req is http or https
+// we want http
+app.use(function (req, res, next) {
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 
 // tell which folder to serve - using app.use
 // add functionality to your express functiona
@@ -9,6 +20,6 @@ var app = express();
 app.use(express.static('public'));
 
 // port - 3000, function ()
-app.listen(3000,function () {
-  console.log('Express server is up on port 3000');
+app.listen(PORT,function () {
+  console.log('Express server is up on PORT ' + PORT);
 });
